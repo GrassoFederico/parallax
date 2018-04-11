779fe0079cf9647627a003b16f66db96e244a904
GrassoFederico <35540107+GrassoFederico@users.noreply.github.com>
2018-04-11 14:17:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TEST</title>
+		<link href="css/parallax.css" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script src="js/parallax.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1> Header </h1>
+		</header>
+		<section id="over">
+			<p> Paragraph 1 </p>
+		</section>
+		<section>
+			<p> Paragraph 2 </p>
+		</section>
+		<footer>
+			<p>Footer</p>
+		</footer>
+	</body>
+</html>
---
+++ js/parallax.js
@@ -0,0 +1,6 @@
+const MAX_SCROLL = $("section")[0].css("height");
+const MIN_SCROLL = 0;
+
+$(window).scroll(function(){
+	
+});
