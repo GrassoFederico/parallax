32aa9eb53395f3c57884146c870a09ac4da1093e
GrassoFederico <35540107+GrassoFederico@users.noreply.github.com>
2018-04-11 17:42:14 +0200

    Add files via upload

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,42 @@
-const MAX_SCROLL = $("section")[0].css("height");
 const MIN_SCROLL = 0;
+const MAX_SCROLL = 949;
+
+var	positionScroll = MIN_SCROLL;
+var	sectionScroll;
+
+function checkScroll(vScroll)
+{
+	if(vScroll < MIN_SCROLL)
+		return (MIN_SCROLL);
+	else if(vScroll > MAX_SCROLL)
+		return (MAX_SCROLL);
+	else
+		return (vScroll);
+}
+
+function getSectionScroll()
+{
+	var tmpPositionScroll = $(this).scrollTop();
+	var tmpSectionTop = $("section#over").css("top");
+	var scrollValue;
+	
+	if(positionScroll < tmpPositionScroll)
+	{
+		scrollValue = parseInt(tmpSectionTop.split("px")[0]) - positionScroll;
+		$("section#over").css("top",checkScroll(scrollValue) + "px");
+	}
+	else
+	{
+		scrollValue = parseInt(tmpSectionTop.split("px")[0]) + (positionScroll * 2);
+		$("section#over").css("top",checkScroll(scrollValue) + "px");
+	}
+	
+	positionScroll = tmpPositionScroll;
+}
 
 $(window).scroll(function(){
 	
+	getSectionScroll();
+	
 });
+
